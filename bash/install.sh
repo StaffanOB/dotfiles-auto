51596f4531a81955fddf076ee254726be82fd2c2
Staffan B <staffan@toot.se>
2023-05-24 22:16:47 +0200

    sett default shell

--- bash/install.sh
+++ bash/install.sh
@@ -2,3 +2,11 @@ echo "BASH"
 COMMAND="bash"
 FILES=("bashrc" "bash_alias" "profile")
 DLINK=("$HOME/.bashrc" "$HOME/.bash_alias" "$HOME/.profile")
+
+# Install package 
+
+echo "     * Installling packages $COMMAND"
+sudo apt install bash -y &> /dev/null
+# Set bash as default shell
+sudo chsh -s /usr/bin/bash $USER
+echo "     * Setting default shell to BASH"
--- vim/install.sh
+++ vim/install.sh
@@ -3,4 +3,4 @@ COMMAND="vim"
 FILES=("vimrc" "viminfo" "")
 DLINK=("$HOME/.vimrc" "$HOME/.viminfo" "$HOME/.vim")
 
-sudo apt install vim-nox &> /dev/null
+
