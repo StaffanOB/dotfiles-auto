4c0e47caccce464bb51a7f45983e6cfa7d410117
Staffan B <staffan@toot.se>
2023-05-24 17:24:45 +0200

    home dir fix

--- bash/install.sh
+++ bash/install.sh
@@ -1,4 +1,4 @@
 echo "BASH"
 COMMAND="bash"
-FILES=('bashrc' 'bash_alias' 'bash_profile')
-DLINK=('/tmp/.bashrc' '/tmp/.bash_alias' '/tmp/.bash_profile')
+FILES=('bashrc' 'bash_alias' 'profile')
+DLINK=('~/.bashrc' '~/.bash_alias' '~/.profile')
--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 echo "VIM"
 COMMAND="vim"
 FILES=('vimrc' 'viminfo' '')
-DLINK=('/tmp/.vimrc' '/tmp/.viminfo' '/tmp/.vim')
+DLINK=('~/.vimrc' '~/.viminfo' '~/.vim')
 
 sudo apt install vim-nox > /dev/null 2&>1
