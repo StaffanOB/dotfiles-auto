ca25c05081004996636c842f0263b8dfad99bc5a
StaffanOB <staffan@toor.se>
2025-05-08 21:20:43 +0200

    added new tmux config

--- tmux/install.sh
+++
@@ -1,8 +0,0 @@
-echo "TMUX" 
-COMMAND="tmux"
-FILES=("tmux.conf")
-DLINK=("$HOME/.tmux.conf")
-
-sudo apt install tmux &> /dev/null 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &> /dev/null 
-tmux source ~/.tmux.conf &> /dev/null 
